f310a21c92a65d8f9428c35d27e956496a740c64
ArjunThazhath <146722372+arjunthazath@users.noreply.github.com>
2025-03-19 23:21:36 +0530

    ok

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
+import { TiThMenuOutline } from "react-icons/ti";
 
 function Navbar() {
   return (
-    <div className='text-white flex items-center px-4'>
+    <div className='text-white bg-amber-300 flex items-center max-w-[1240px] mx-auto px-4'>
         <h1 className='w-full text-3xl font-bold text-red-500'>Navbar</h1>
-        <ul className='flex'>
+        <ul className='flex hidden'>
             <li className='p-4'>HOME</li>
             <li className='p-4'>COMPANY</li>
             <li className='p-4'>RESOURCES</li>
         </ul>
+        <div>
+            <TiThMenuOutline size={20}/>
+        </div>
+        <div className='fixed left-0 top-0 bg-gray-600 w-[60%] h-full border-y-8 border-r-8 rounded-r-2xl border-cyan-300'>
+        <h1 className='w-full text-3xl font-bold text-red-500 m-6'>Navbar</h1>
+            <ul className='p-6 uppercase'>
+                <li className='p-4'>HoME</li>
+                <li className='p-4'>COmpaNY</li>
+                <li className='p-4'>RESOURCES</li>
+            </ul>
+        </div>
     </div>
   )
 }
