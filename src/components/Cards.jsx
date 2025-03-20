1074a6f4663c2697230b7a69ce73030c655bc5f9
ArjunThazhath <146722372+arjunthazath@users.noreply.github.com>
2025-03-21 03:11:39 +0530

    pricing

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,23 +2,44 @@ import React from 'react'
 import One from '../assets/oneman.png'
 function Cards() {
   return (
-    <div className='w-full bg-white p-10'>
-        <div className='grid min-h-[400px] mx-auto gap-7 md:grid-cols-3'>
-            <div className='md:col-span-1'>
-                <img src={One} className='w-[100px] mx-auto z-10'/>
-                <div className='border-black'>
-                    <h1>Single User</h1>
-                    <h1>$149</h1>
-                    <div className='text-black font-bold'>
-                        <h3 className='border-t-[1px] border-black'>500 GB Storage</h3>
-                        <h3 className='border-y-[1px] border-black'>1 User Allowed</h3>
-                        <h3 className='border-b-[1px] border-black'>Send up to 2GB</h3>
+    <div className='w-full bg-white p-20'>
+        <div className='grid min-h-[400px] mx-auto gap-7 md:grid-cols-3 pt-10'>
+            <div className='md:col-span-1 shadow-xl flex flex-col rounded-2xl hover:scale-105 duration-300 my-8 md:my-0'>
+                <img src={One} className='w-[60px] bg-white mx-auto mt-[-3rem]'/>
+                    <h1 className='font-bold text-center text-2xl py-8'>Single User</h1>
+                    <h1  className='font-bold  text-center text-4xl'>$149</h1>
+
+                    <div className='text-center font-medium'>
+                        <h3 className='border-t-[1px] border-black p-2 mt-8 mx-8'>500 GB Storage</h3>
+                        <h3 className='border-y-[1px] p-2 border-black mx-8'>1 User Allowed</h3>
+                        <h3 className='border-b-[1px] p-2 border-black mx-8'>Send up to 2GB</h3>
                     </div>
-                    <button className='bg-green-400 text-xl font-bold text-black rounded-md px-5 py-3'>Start Trial</button>
-                </div>
+                    <button className='bg-green-400 mx-auto text-xl font-bold text-black rounded-md px-3 w-[200px] py-2 my-6'>Start Trial</button>
             </div>
-            <div className='md:col-span-1 bg-blue-500'>CARD2</div>
-            <div className='md:col-span-1 bg-green-400'>CARD3</div>
+            <div className='md:col-span-1 my-8 md:my-0 shadow-xl flex flex-col rounded-2xl hover:scale-105 duration-300 bg-gray-200'>
+                <img src={One} className='w-[60px] mx-auto mt-[-3rem]'/>
+                    <h1 className='font-bold text-center text-2xl py-8'>Single User</h1>
+                    <h1  className='font-bold  text-center text-4xl'>$149</h1>
+
+                    <div className='text-center font-medium'>
+                        <h3 className='border-t-[1px] border-black p-2 mt-8 mx-8'>500 GB Storage</h3>
+                        <h3 className='border-y-[1px] p-2 border-black mx-8'>1 User Allowed</h3>
+                        <h3 className='border-b-[1px] p-2 border-black mx-8'>Send up to 2GB</h3>
+                    </div>
+                    <button className='bg-green-400 mx-auto text-xl font-bold text-black rounded-md px-3 w-[200px] py-2 my-6'>Start Trial</button>
+            </div>
+            <div className='md:col-span-1 shadow-xl my-8 md:my-0 flex flex-col rounded-2xl hover:scale-105 duration-300'>
+                <img src={One} className='w-[60px] bg-white mx-auto mt-[-3rem]'/>
+                    <h1 className='font-bold text-center text-2xl py-8'>Single User</h1>
+                    <h1  className='font-bold  text-center text-4xl'>$149</h1>
+
+                    <div className='text-center font-medium'>
+                        <h3 className='border-t-[1px] border-black p-2 mt-8 mx-8'>500 GB Storage</h3>
+                        <h3 className='border-y-[1px] p-2 border-black mx-8'>1 User Allowed</h3>
+                        <h3 className='border-b-[1px] p-2 border-black mx-8'>Send up to 2GB</h3>
+                    </div>
+                    <button className='bg-green-400 mx-auto text-xl font-bold text-black rounded-md px-3 w-[200px] py-2 my-6'>Start Trial</button>
+            </div>
         </div>
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,7 +26,7 @@ function Navbar() {
         <div onClick={handleNav} className='inline md:hidden'>
             {nav?<IoMdCloseCircleOutline size={20}/>: <TiThMenuOutline size={20}/>}
         </div>
-        <div className={nav?'fixed left-0 top-0 bg-gray-600 w-[60%] h-full border-y-8 border-r-8 rounded-r-2xl border-cyan-300 ease-in-out duration-500': 'fixed left-[-100%]'}>
+        <div className={nav?'fixed left-0 top-0 bg-black w-[60%] h-full border-y-8 border-r-8 rounded-r-2xl ease-in-out duration-500': 'fixed left-[-100%]'}>
         <h1 className='w-full text-3xl font-bold text-white m-6'>Navbar</h1>
             <ul className='p-6 uppercase'>
                 <li className='p-4 border-b border-gray-400'>HoME</li>
